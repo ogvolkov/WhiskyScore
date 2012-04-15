cea02a968d37f148dd937a878026b2706caa448f
Oleg Volkov <OGVolkov@gmail.com>
2012-04-15 15:49:25 +0300

    Primitive parsing whisky scores from ClassicDram PDF. Multiline whisky descriptions are ignored.

--- WhiskyScoreParser/src/WhiskyScoreParser/ClassicDramParser.java
+++ WhiskyScoreParser/src/WhiskyScoreParser/ClassicDramParser.java
@@ -2,38 +2,39 @@ package WhiskyScoreParser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfName;
-import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.pdf.parser.*;
 
 public class ClassicDramParser {
-	public void Parse(InputStream stream) throws IOException{	
-		PdfReader pdfReader = null;		
+	private ArrayList<WhiskyScore> scores = new ArrayList<WhiskyScore>();
+	
+	public void parse(InputStream stream) throws IOException{
+		scores.clear();
+		PdfReader pdfReader = null;						
 		
 		try{
 			pdfReader = new PdfReader(stream);
 			OrderedPdfTextListener textListener = new OrderedPdfTextListener(5.0f);
 			PdfContentStreamProcessor contentProcessor = new PdfContentStreamProcessor(textListener);
 			
-			int pages = 2/*pdfReader.getNumberOfPages()*/;
+			int pages = pdfReader.getNumberOfPages();
 						
 			for(int page=2; page <= pages; page++){
-				textListener.clear();
-				
+				// retrieve page's data
 				PdfDictionary pageDictionary = pdfReader.getPageN(page);
-				PdfDictionary resourceDictionary = pageDictionary.getAsDict(PdfName.RESOURCES);
-				
+				PdfDictionary resourceDictionary = pageDictionary.getAsDict(PdfName.RESOURCES);				
 				byte[] pageData = ContentByteUtils.getContentBytesForPage(pdfReader, page);
+				
+				// parse page data into text lines
+				textListener.clear();
 				contentProcessor.processContent(pageData, resourceDictionary);
 				
-				for(Iterable<String> line: textListener.getLines()){
-					for(String text: line){
-						System.out.print(text);
-						System.out.print(" ");
-					}
-					System.out.println();
+				// parse whisky scores from found lines				
+				for(List<String> line: textListener.getLines()){
+					processLine(line);
 				}
 			}
 		}
@@ -43,9 +44,43 @@ public class ClassicDramParser {
 				pdfReader.close();
 			}
 		}
+	}	
+	
+	public Iterable<WhiskyScore> getScores(){
+		return scores;		
 	}
 	
+	private void processLine(List<String> line){
+		if (line.size() < 2) return;
+		
+		String firstCell = line.get(0);	
+		if (!isFloat(firstCell)) return; // TODO: handle multi-line cells
+		
+		String name = line.get(1);
+		// temp hack: exclude incorrectly parsed strings
+		if (!isCorrectWhiskyName(name)) return;
+		
+		if(line.size() > 2){
+			String description = line.get(2);
+			if (description != null){
+				name = name + " " + description;
+			}
+		}
+		
+		scores.add(new WhiskyScore(name, firstCell));
+	}
 	
-
+	private static Boolean isFloat(String text){		
+		try{
+			Float.parseFloat(text.replace(',', '.'));
+			return true;
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+	}
+	
+	private static Boolean isCorrectWhiskyName(String text){		
+		return Character.isUpperCase(text.charAt(0)) &&Character.isUpperCase(text.charAt(1));
+	}	
 }
-
---
+++ WhiskyScoreParser/src/WhiskyScoreParser/WhiskyScore.java
@@ -0,0 +1,21 @@
+package WhiskyScoreParser;
+
+public class WhiskyScore {
+	private String name;
+	
+	private String ratingString;
+	
+	public WhiskyScore(String name, String ratingString)
+	{
+		this.name = name;
+		this.ratingString = ratingString;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getRating() {
+		return ratingString;
+	}
+}
